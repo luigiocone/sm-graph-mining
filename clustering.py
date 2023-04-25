5b25995fbd4cbd49c9b9305dd3f30b8d897d5330
Luigi Ocone <ocone.luigi123@gmail.com>
2023-04-25 18:39:40 +0200

    refactoring and bug fixed

--- clustering.py
+++ clustering.py
@@ -1,49 +1,60 @@
-import scipy.cluster.hierarchy as sch
-import matplotlib.pyplot as plt
-import pandas as pd
+import os
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as sch
 
 MAX_DISTANCE = 0.08
+CLUSTERING_FOLDER = "clustering_data"
+CLUSTERING_FILE = os.path.join(CLUSTERING_FOLDER, "clusters.json")
 
 def min_max_normalization(df):
     min = df.min()
     max = df.max()
     return ((df - min) / (max - min))
 
-df = pd.read_csv(filepath_or_buffer="0.netsummary.csv")
-df['num_nodes'] = min_max_normalization(df['num_nodes'])
-df['num_edges'] = min_max_normalization(df['num_edges'])
+def get_clusters_dict(linkage_matrix, max_distance=MAX_DISTANCE):
+    # Set the threshold for the maximum distance between points (used to cut the tree)
+    clusters = sch.fcluster(linkage_matrix, max_distance, criterion='distance')
+    
+    # Create a dictionary of clusters and the elements belonging to each cluster
+    cluster_dict = {}
+    for i, cluster_id in enumerate(clusters):
+        if cluster_id not in cluster_dict:
+            cluster_dict[int(cluster_id)] = []
+        cluster_dict[int(cluster_id)].append(i)
+    return cluster_dict
 
-# Compute the distance matrix using the Euclidean distance metric
-data = df[['num_nodes', 'num_edges']]
-dist_matrix = sch.distance.pdist(data, metric='euclidean')
-
-# Perform hierarchical clustering using the computed distance matrix and chosen linkage method
-linkage_matrix = sch.linkage(dist_matrix, method='centroid')
-
-# Plot the dendrogram and the cut-line to visualize the results
-dendrogram = sch.dendrogram(linkage_matrix)
-plt.axhline(y=MAX_DISTANCE, color='r', linestyle='-')
-plt.show()
-
-# get the cluster assignments
-# set the threshold for the maximum distance between points
-clusters = sch.fcluster(linkage_matrix, MAX_DISTANCE, criterion='distance')
-
-# create a dictionary of clusters and the elements belonging to each cluster
-cluster_dict = {}
-for i, cluster in enumerate(clusters):
-    if cluster not in cluster_dict:
-        cluster_dict[int(cluster)] = []
-    cluster_dict[int(cluster)].append(i)
-
-# Serialize data into file:
-cluster_dict = dict(sorted(cluster_dict.items()))
-with open("clusters.json", 'w') as output:
+if __name__ == '__main__':
+    if not os.path.exists(CLUSTERING_FOLDER):
+        os.mkdir(CLUSTERING_FOLDER)
+    df = pd.read_csv(filepath_or_buffer="0.netsummary.csv")
+    
+    # Normalize dataframe and save it to csv
+    df['num_nodes_norm'] = min_max_normalization(df['num_nodes'])
+    df['num_edges_norm'] = min_max_normalization(df['num_edges'])
+    df.to_csv(path_or_buf=os.path.join(CLUSTERING_FOLDER, 'netsummary_normalized.csv'))
+    
+    # Compute the distance matrix using the Euclidean distance metric
+    data = df[['num_nodes_norm', 'num_edges_norm']]
+    dist_matrix = sch.distance.pdist(data, metric='euclidean')   
+    
+    # Perform hierarchical clustering using the computed distance matrix and centroid linkage method
+    linkage_matrix = sch.linkage(dist_matrix, method='centroid')
+    
+    # Plot the dendrogram and the distance threshold to visualize the results
+    dendrogram = sch.dendrogram(linkage_matrix)
+    plt.axhline(y=MAX_DISTANCE, color='r', linestyle='-')
+    plt.show()
+    
+    # Get clusters at MAX_DISTANCE
+    cluster_dict = get_clusters_dict(linkage_matrix)
+    cluster_dict = dict(sorted(cluster_dict.items()))
     string = json.dumps(cluster_dict)
-    # A prittier way to dump the json file
+    # A pretty way to dump this json file
     string = string.replace('], ', '],\n\t')
     string = string.replace('{', '{\n\t')
     string = string.replace('}', '\n}')
-    output.write(string)
+    with open(os.path.join(CLUSTERING_FOLDER, "clusters.json"), 'w') as output:
+        output.write(string)
 
--- plotter.py
+++ plotter.py
@@ -1,6 +1,9 @@
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
-from clusters import clusters
+from clustering import CLUSTERING_FILE
+
+NUM_CLUSTER = 5
 
 # Marker color pairs
 mc_pairs = [
@@ -11,7 +14,7 @@ mc_pairs = [
     ['s', 'tab:red'],
     ['+', 'tab:purple'],
     ['P', 'tab:brown'],
-
+    
     ['*', 'tab:gray'],
     ['x', 'tab:olive'],
     ['X', 'tab:cyan']
@@ -20,12 +23,20 @@ mc_pairs = [
 df = pd.read_csv(filepath_or_buffer="metrics.csv", delimiter=',')
 df['file'] = df['file'].str[13:-4]
 
-for i in range(6):
-    metric = df.columns[i+1]
-    for i in range(len(mc_pairs)):
-        mask = df['file'].isin(clusters[i])
-        temp_df = df[mask]
-        plt.scatter(temp_df['file'], temp_df[metric], marker=mc_pairs[i][0], c=mc_pairs[i][1])
+# Picking the 5 largest clusters 
+with open(CLUSTERING_FILE) as file:
+    clusters = json.load(file)
+clusters = list(clusters.values())
+clusters.sort(key = len, reverse=True)
+clusters = clusters[:NUM_CLUSTER]
+
+# For each metric
+for i in range(1, 7):
+    metric = df.columns[i]
+    # For each cluster
+    for j in range(NUM_CLUSTER):
+        temp_df = df.filter(items=clusters[j], axis=0)
+        plt.scatter(temp_df['file'], temp_df[metric], marker=mc_pairs[j][0], c=mc_pairs[j][1])
     plt.xticks(rotation=90)
     plt.title(metric)
     plt.subplots_adjust(left=0.065, right=0.945, top=0.95, bottom=0.145)
